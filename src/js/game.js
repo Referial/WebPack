b265959bc917bae976bcc666edf100d2be0996f3
Nikita M <MayorovNikita7@yandex.ru>
2024-09-06 17:58:12 +0500

    первая фиксация

---
+++ src/js/game.js
@@ -0,0 +1,16 @@
+import "./domain.js";
+
+export default class Game {
+    start() {
+        console.log('game started');
+    }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
